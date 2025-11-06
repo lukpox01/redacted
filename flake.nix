d6b28f0787597dbb230eea17dc0b731da23d07d6
LukasPolacek <lukiy223344@gmail.com>
2025-11-06 15:47:36 +0100

    Initial project setup with Nix and Rust

---
+++ flake.nix
@@ -0,0 +1,39 @@
+
+{
+  description = "A simple Rust project with a stable toolchain";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    # This makes your flake compatible with different systems (Linux, macOS) [web:3][web:5]
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Apply the rust-overlay to your Nix packages [attached_file:1]
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        # This defines the development shell that `nix develop` will use
+        devShells.default = with pkgs; mkShell {
+          # These are the packages that will be available in your shell [attached_file:1]
+          buildInputs = [
+            # The full, stable Rust toolchain [attached_file:1]
+            rust-bin.stable.latest.default
+
+            # Common dependencies for Rust projects
+            openssl
+            pkg-config
+          ];
+        };
+      }
+    );
+}

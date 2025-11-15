150f1a097ce0cedf146c5e944aff75572c708519
Lukas Polacek <lukaspolacek.it@gmail.com>
2025-11-15 17:00:22 +0100

    clean-up into functions handle_checkin/beacon requests

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use serde::Deserialize;
 use std::collections::{HashMap, VecDeque};
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use std::ops::Index;
 use std::sync::{Arc, Mutex};
 use tokio::net::TcpListener;
 use uuid::Uuid;
@@ -57,109 +56,104 @@ impl State {
     }
 }
 
+fn handle_checkin(
+    data: CheckIn,
+    state: Arc<Mutex<State>>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
+    let agent_id = {
+        let mut guard = state.lock().unwrap();
+        let agent = Agent {
+            os: data.os,
+            hostname: data.hostname,
+            mac: data.mac,
+            ip: data.ip,
+        };
+        let uuid_v4 = Uuid::new_v4();
+        guard.agents.insert(uuid_v4, agent);
+        uuid_v4
+    };
+    println!("{:#?}", Arc::clone(&state).lock().unwrap().agents);
+    Ok(Response::new(Full::new(Bytes::from(agent_id.to_string()))))
+}
+
+fn handle_beacon(
+    data: Beacon,
+    state: Arc<Mutex<State>>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
+    println!("Received beacon: {:?}", data);
+    let agent = {
+        let guard = state.lock().unwrap();
+        let uuid_v4 = match Uuid::parse_str(data.id.as_str()) {
+            Ok(val) => val,
+            Err(_) => {
+                let mut bad_request = Response::new(Full::new(Bytes::from(
+                    "Bad Request: Invalid format of Uuid",
+                )));
+                *bad_request.status_mut() = StatusCode::BAD_REQUEST;
+                return Ok(bad_request);
+            }
+        };
+        match guard.agents.get(&uuid_v4) {
+            Some(agent) => Agent {
+                os: agent.os.clone(),
+                hostname: agent.hostname.clone(),
+                mac: agent.mac.clone(),
+                ip: agent.ip.clone(),
+            },
+            None => {
+                let mut bad_request =
+                    Response::new(Full::new(Bytes::from("Bad Request: Invalid AgentId")));
+                *bad_request.status_mut() = StatusCode::BAD_REQUEST;
+                return Ok(bad_request);
+            }
+        }
+    };
+    Ok(Response::new(Full::new(Bytes::from(format!(
+        "agent found thanks to you \n{:#?}",
+        agent
+    )))))
+}
+
 async fn handle_request(
     req: Request<hyper::body::Incoming>,
     state: Arc<Mutex<State>>,
 ) -> Result<Response<Full<Bytes>>, Infallible> {
-    match (req.method(), req.uri().path()) {
+    let (req_method, req_path) = (req.method().clone(), &req.uri().path().to_owned());
+
+    let body_bytes = match req.collect().await {
+        Ok(collected) => collected.to_bytes(),
+        Err(_) => {
+            let mut bad_request = Response::new(Full::new(Bytes::from("Internal Server Error")));
+            *bad_request.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+            return Ok(bad_request);
+        }
+    };
+
+    match (&req_method, req_path.as_str()) {
         (&Method::POST, "/checkin") => {
-            // get the request into bytes for serde
-            let body_bytes = match req.collect().await {
-                Ok(collected) => collected.to_bytes(),
-                Err(_) => {
-                    let mut bad_request =
-                        Response::new(Full::new(Bytes::from("Internal Server Error")));
-                    *bad_request.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
-                    return Ok(bad_request);
-                }
-            };
-
-            let check_in: Result<CheckIn, _> = serde_json::from_slice(&body_bytes);
-
-            match check_in {
-                Ok(parsed_content) => {
-                    let agent_id = {
-                        let mut guard = state.lock().unwrap();
-                        let agent = Agent {
-                            os: parsed_content.os,
-                            hostname: parsed_content.hostname,
-                            mac: parsed_content.mac,
-                            ip: parsed_content.ip,
-                        };
-                        let uuid_v4 = Uuid::new_v4();
-                        guard.agents.insert(uuid_v4, agent);
-                        uuid_v4
-                    };
-                    println!("{:#?}", Arc::clone(&state).lock().unwrap().agents);
-                    Ok(Response::new(Full::new(Bytes::from(agent_id.to_string()))))
-                }
+            let check_in: CheckIn = match serde_json::from_slice(&body_bytes) {
+                Ok(data) => data,
                 Err(_) => {
                     let mut bad_request =
                         Response::new(Full::new(Bytes::from("Bad Request: Invalid Json")));
                     *bad_request.status_mut() = StatusCode::BAD_REQUEST;
-                    Ok(bad_request)
+                    return Ok(bad_request);
                 }
-            }
+            };
+            handle_checkin(check_in, Arc::clone(&state))
         }
-
         (&Method::POST, "/beacon") => {
-            // 1. Collect the body using `BodyExt::collect()`. This replaces `to_bytes`.
-            let body_bytes = match req.collect().await {
-                Ok(collected) => collected.to_bytes(),
+            let beacon: Beacon = match serde_json::from_slice(&body_bytes) {
+                Ok(data) => data,
                 Err(_) => {
                     let mut bad_request =
-                        Response::new(Full::new(Bytes::from("Internal Server Error")));
-                    *bad_request.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+                        Response::new(Full::new(Bytes::from("Bad Request: Invalid Json")));
+                    *bad_request.status_mut() = StatusCode::BAD_REQUEST;
                     return Ok(bad_request);
                 }
             };
 
-            let beacon: Result<Beacon, _> = serde_json::from_slice(&body_bytes);
-
-            match beacon {
-                Ok(parsed_beacon) => {
-                    println!("Received beacon: {:?}", parsed_beacon);
-                    let agent = {
-                        let guard = state.lock().unwrap();
-                        let uuid_v4 = match Uuid::parse_str(parsed_beacon.id.as_str()) {
-                            Ok(val) => val,
-                            Err(_) => {
-                                let mut bad_request = Response::new(Full::new(Bytes::from(
-                                    "Bad Request: Invalid format of Uuid",
-                                )));
-                                *bad_request.status_mut() = StatusCode::BAD_REQUEST;
-                                return Ok(bad_request);
-                            }
-                        };
-                        match guard.agents.get(&uuid_v4) {
-                            Some(agent) => Agent {
-                                os: agent.os.clone(),
-                                hostname: agent.hostname.clone(),
-                                mac: agent.mac.clone(),
-                                ip: agent.ip.clone(),
-                            },
-                            None => {
-                                let mut bad_request = Response::new(Full::new(Bytes::from(
-                                    "Bad Request: Invalid AgentId",
-                                )));
-                                *bad_request.status_mut() = StatusCode::BAD_REQUEST;
-                                return Ok(bad_request);
-                            }
-                        }
-                    };
-                    Ok(Response::new(Full::new(Bytes::from(format!(
-                        "agent found thanks to you \n{:#?}",
-                        agent
-                    )))))
-                }
-                Err(e) => {
-                    println!("Failed to parse beacon: {}", e);
-                    let mut bad_request =
-                        Response::new(Full::new(Bytes::from("Bad Request: Invalid JSON")));
-                    *bad_request.status_mut() = StatusCode::BAD_REQUEST;
-                    Ok(bad_request)
-                }
-            }
+            handle_beacon(beacon, Arc::clone(&state))
         }
 
         _ => {
